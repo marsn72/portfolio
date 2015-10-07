dc073cb3029b1244a32be41c50ee2b2c4cafdd9a
marsn72 <my_new_pochta@bk.ru>
2015-10-08 02:47:53 +0300

    Первая страница сходится с PerfectPixel

---
+++ app/js/script.js
@@ -0,0 +1,49 @@
+/*
+$('.add-project-btn').on('click', _showModal);
+
+var _showModal = function(){
+    $('.add-project-popup').bPopup({
+        speed: 650,
+        transition: 'slideIn',
+        transitionClose: 'slideBack'
+    });
+}*/
+var myModal = (function(){
+
+
+})();
+
+myModal.init()
+
+// Объявление модуля
+var modalWindow = (function () {
+
+    // Инициализирует наш модуль
+    var init = function () {
+        _setUpListners();
+    };
+
+    // Прослушивает события
+    var _setUpListners = function () {
+        $('#add-new-item').on('click', _showModal);
+    };
+
+    var _showModal = function  () {
+        $('#new-project-popup').bPopup({
+            speed: 1650,
+            transition: 'slideDown',
+            /*onClose: function () {
+                this.find('.form')
+                    .trigger("reset");
+            }*/
+        });
+    }
+    // Возвращаем объект (публичные методы)
+    return {
+        init: init
+    };
+
+})();
+
+// Вызов модуля
+modalWindow.init();
